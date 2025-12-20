2491bfa0840228414312d8c25f32fcb07f1de223
Oleksandr-Liashkov <aleksandr.liashkov98@gmail.com>
2025-12-20 17:27:30 +0000

    Deploying to gh-pages from @ Oleksandr-Liashkov/goit-js-hw-09@6357ee891e81128727d68f0f0942245a8b4ec610 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -7,5 +7,5 @@ import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                
     />
   </a>
 </li>
-`).join("");a.insertAdjacentHTML("afterbegin",t);new p(".gallery a",{captions:!0,captionsData:"alt",сaptionPosition:"bottom",captionDelay:250});
+`).join("");a.insertAdjacentHTML("afterbegin",t);new p(".gallery a",{captions:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
